f33e1a468a58530f6422bfb00320764a57a7ad19
Maicol Battistini <maicolbattistini@live.it>
2024-12-03 16:30:45 +0100

    refactor: 🧹 Remove unused imports from App.kt
    This commit removes unused imports from the `App.kt` file, cleaning up the codebase.
    
    Specific changes include:
    
    - Removed imports for `CircularProgressIndicator`, `LazyHorizontalGrid`, `Star`, `pulltorefresh.rememberPullToRefreshState`.

--- sample/composeApp/src/commonMain/kotlin/it/maicol07/spraypaintkt/sample/App.kt
+++ sample/composeApp/src/commonMain/kotlin/it/maicol07/spraypaintkt/sample/App.kt
@@ -12,16 +12,13 @@ import androidx.compose.foundation.layout.safeDrawing
 import androidx.compose.foundation.layout.windowInsetsPadding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.grid.GridCells
-import androidx.compose.foundation.lazy.grid.LazyHorizontalGrid
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Refresh
-import androidx.compose.material.icons.filled.Star
 import androidx.compose.material.icons.outlined.Star
 import androidx.compose.material3.Card
-import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
@@ -32,7 +29,6 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.ModalBottomSheet
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.material3.pulltorefresh.rememberPullToRefreshState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
