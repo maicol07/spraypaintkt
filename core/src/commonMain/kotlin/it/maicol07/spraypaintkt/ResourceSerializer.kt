845a4be9986534fe68767b32bd93bb3233f739ce
Maicol Battistini <maicolbattistini@live.it>
2024-12-30 15:37:43 +0100

    refactor: ✨ Make ResourceSerializer generic
    This commit refactors the `ResourceSerializer` to be generic, allowing for more flexibility in serialization and deserialization of resources.
    
    Specific changes include:
    
    - Made `ResourceSerializer` an abstract class with a type parameter `R` extending `Resource`.
    - Added a private object `Serializer` within `Resource` to handle serialization.
    - Updated `serialize()` and `deserialize()` methods to use the generic type `R`.
    - Modified the `deserialize()` method to handle both single and collection responses, and to ensure the deserialized resource is of type `R`.

--- core/src/commonMain/kotlin/it/maicol07/spraypaintkt/ResourceSerializer.kt
+++ core/src/commonMain/kotlin/it/maicol07/spraypaintkt/ResourceSerializer.kt
@@ -7,21 +7,25 @@ import kotlinx.serialization.descriptors.PrimitiveSerialDescriptor
 import kotlinx.serialization.encoding.Decoder
 import kotlinx.serialization.encoding.Encoder
 
-object ResourceSerializer: KSerializer<Resource> {
+abstract class ResourceSerializer<R: Resource>: KSerializer<R> {
     override val descriptor = PrimitiveSerialDescriptor("Resource", PrimitiveKind.STRING)
 
-    override fun serialize(encoder: Encoder, value: Resource) {
+    override fun serialize(encoder: Encoder, value: R) {
         encoder.encodeString(value.toJsonApiString())
     }
 
-    override fun deserialize(decoder: Decoder): Resource {
+    override fun deserialize(decoder: Decoder): R {
         val jsonApiString = decoder.decodeString().trim('"')
+
         val response = JsonApiSingleResponse.fromJsonApiString(jsonApiString)
-        if (response.data == null) {
-            val resource = JsonApiResource.fromJsonApiString(jsonApiString)
-            return Deserializer().deserialize(resource)
+        val resource = if (response.data == null) {
+            val response = JsonApiResource.fromJsonApiString(jsonApiString)
+            Deserializer().deserialize(response)
+        } else {
+            Deserializer().deserialize(response)
         }
 
-        return Deserializer().deserialize(response)
+        @Suppress("UNCHECKED_CAST") // We know it's a resource
+        return resource as? R ?: throw IllegalArgumentException("Resource is not of type R")
     }
 }
